fe4f3f150cca4bffcb68e2c41a18cd2bbb8e1b29
cyj-12138 <70762697+cyj-12138@users.noreply.github.com>
2020-09-17 00:31:24 +0800

    修改后的代码

--- main.py
+++ main.py
@@ -2,23 +2,26 @@
 # 分词包
 import jieba
 import jieba.analyse
+# 机器学习包 sklearn中的cosine_similarity用于计算余弦相似度
 from sklearn.metrics.pairwise import cosine_similarity
+# 用于读取命令行参数
 import sys
 
 
 class CosineSimilarity(object):
+    # 对CosineSimilarity的构造
     def __init__(self, file1, file2):
         self.s1 = file1
         self.s2 = file2
 
-    @staticmethod
+    @staticmethod   # 静态函数
     def get_keyword(content):  # 提取关键词
         seg = [i for i in jieba.cut(content, cut_all=True) if i != '']  # 分词
+        # 按照权重返回前topK个关键词
         keywords = jieba.analyse.extract_tags("|".join(seg), topK=K, withWeight=False)
         return keywords
 
-
-    @staticmethod
+    @staticmethod   # oneHot编码
     def one_hot(keyword_dict, keywords):
         cut_code = [0] * len(keyword_dict)
         for word in keywords:
@@ -26,20 +29,24 @@ class CosineSimilarity(object):
         return cut_code
 
     def calculate(self):
+        # 提取关键词
         keywords1 = self.get_keyword(self.s1)
         keywords2 = self.get_keyword(self.s2)
+        # 词的并集
         union = set(keywords1).union(set(keywords2))
+        # 构造hash表
         keyword_dict = {}
         i = 0
         for word in union:
             keyword_dict[word] = i
             i += 1
-
+        # oneHot编码
         vector1 = self.one_hot(keyword_dict, keywords1)
         vector2 = self.one_hot(keyword_dict, keywords2)
         sample = [vector1, vector2]
+        # 除0处理
         try:
-            simRate = cosine_similarity(sample)
+            simRate = cosine_similarity(sample)   # 用sklearn自带的余弦相似度计算
             return simRate[1][0]
         except Exception as e:
             print(e)
@@ -48,27 +55,31 @@ class CosineSimilarity(object):
 
 # 测试
 if __name__ == '__main__':
-    rootPath = sys.argv[1]
-    copyPath = sys.argv[2]
-    ansPath = sys.argv[3]
+    # 命令行输入绝对路径
+    root_Path = sys.argv[1]
+    copy_Path = sys.argv[2]
+    ans_Path = sys.argv[3]
+    # 读入两个文本 计算topK
     try:
-        with open(rootPath, encoding='UTF-8') as fp:
+        with open(root_Path, encoding='UTF-8') as fp:
             root = fp.read()
             seg = [i for i in jieba.cut(root, cut_all=True) if i != '']
         K = int(len(seg) / 8)
     except:
         K = 0
     try:
-        with open(rootPath, encoding='UTF-8') as fp:
+        with open(root_Path, encoding='UTF-8') as fp:
             ori = fp.read()
-        with open(copyPath, encoding='UTF-8') as fp:
+        with open(copy_Path, encoding='UTF-8') as fp:
             copy = fp.read()
     except Exception as e:
         print(e)
     model = CosineSimilarity(ori, copy)
+    # 保留两位小数
     similarity = round(model.calculate(), 2)
+    # 输出答案到文本
     try:
-        with open(ansPath, "w+", encoding='UTF-8') as fp:
+        with open(ans_Path, "w+", encoding='UTF-8') as fp:
             fp.write(str(similarity))
     except Exception as e:
         print(e)
